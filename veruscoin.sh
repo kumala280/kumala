187e524a97f6ae79fe54a8f24932c19b1bd5636f
kumala280 <raisakumala03@gmail.com>
2024-12-27 12:52:24 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar fx hellminer_linux64.tar.gz
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RHxNUfQwfKsX5uVZGjitavaqvMCPsrA4pS.Rig002 -p hybrid
 while [ 1 ]; do
 sleep 3
